44399b627b131b6391b7f268fef09e80943e64f3
Joezari Borlongan <bor.joezari@gmail.com>
2026-03-06 12:05:24 -0800

    perf: self-host Nunito font to eliminate external request chain
    Replace Google Fonts CDN link with @fontsource/nunito package.
    This eliminates 2 cross-origin round-trips (fonts.googleapis.com CSS
    + fonts.gstatic.com woff2) that were delaying First Contentful Paint.
    
    - Install @fontsource/nunito, import only latin 400/500/600/700
    - Remove Google Fonts <link> and preconnect tags from index.html
    - Drop unused 800 weight

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryProvider } from "@/providers/query-provider";
+import "@fontsource/nunito/latin-400.css";
+import "@fontsource/nunito/latin-500.css";
+import "@fontsource/nunito/latin-600.css";
+import "@fontsource/nunito/latin-700.css";
 import "./index.css";
 import App from "./App.tsx";
 
