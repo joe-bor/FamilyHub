0ec2124de0d2bdaf7a4d9b98d01c372a9bf40832
Joezari Borlongan <bor.joezari@gmail.com>
2026-03-21 04:06:47 -0700

    feat(settings): add Google Calendar connection section to member profile

--- src/components/settings/index.ts
+++ src/components/settings/index.ts
@@ -1,3 +1,4 @@
 export { FamilySettingsModal } from "./family-settings-modal";
 export { GoogleCalendarPickerModal } from "./google-calendar-picker-modal";
+export { GoogleCalendarSection } from "./google-calendar-section";
 export { MemberProfileModal } from "./member-profile-modal";
