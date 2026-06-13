6e1f783abe08d1cd05db76002e67ae3166bb27e2
Joezari Borlongan <bor.joezari@gmail.com>
2026-06-13 07:04:00 -0700

    feat(pwa): prompt before applying service worker updates instead of silent reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { PWAUpdater } from "@/components/pwa/pwa-updater";
 import { QueryProvider } from "@/providers/query-provider";
 import "@fontsource/nunito/latin-400.css";
 import "@fontsource/nunito/latin-500.css";
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
       <App />
+      <PWAUpdater />
     </QueryProvider>
   </StrictMode>,
 );
