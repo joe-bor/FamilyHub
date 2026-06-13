4e44cf02bce63b65a11b31449d1d1f796356ad83
Joezari Borlongan <bor.joezari@gmail.com>
2026-06-13 07:11:56 -0700

    feat(hooks): add useInstallPrompt

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useDebounce } from "./use-debounce";
 export { useGoogleAuthReturn } from "./use-google-auth-return";
+export { useInstallPrompt } from "./use-install-prompt";
 export { useIsMobile } from "./use-is-mobile";
 export { useMediaQuery } from "./use-media-query";
