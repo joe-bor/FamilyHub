c9afb4a42c4ff21df662178a49fb6720406a6b25
Joezari Borlongan <bor.joezari@gmail.com>
2026-06-17 02:52:29 -0700

    feat(motion): add ScreenTransition primitive

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -4,5 +4,6 @@ export { MobileBottomNav } from "./mobile-bottom-nav";
 export { NavigationTabs, type TabType } from "./navigation-tabs";
 export { OfflineBanner } from "./offline-banner";
 export { OfflineUnavailable } from "./offline-unavailable";
+export { ScreenTransition } from "./screen-transition";
 export { SidebarMenu } from "./sidebar-menu";
 export { ThemeProvider } from "./theme-provider";
